87e02abe6b683a14ac162f3f3150678b3290cb96
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-05-24 17:15:44 +0200

    some progress

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(link_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+private
+
+def link_params
+  params.require(:list).permit(:name)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show", as: "list"
+  # post "lists", to: "lists#create"
+  # get "lists/:list_id/bookmarks/new", to: "bookmarks#new", as: "bookmarks_new"
+
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
